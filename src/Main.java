8c3781dd89055a863cfdde5439258f5832e5faa1
Dekoder-py <kyleyesmariam@icloud.com>
2025-06-03 09:45:30 +1200

    add more operations

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main {
         String mode = sc.next();
         switch (mode) {
             case "+" -> add(sc);
-//            case "-" -> subtract(sc);
-//            case "*" -> multiply(sc);
-//            case "/" -> divide(sc);
+            case "-" -> subtract(sc);
+            case "*" -> multiply(sc);
+            case "/" -> divide(sc);
             default -> System.out.println("Invalid input. Goodbye.");
         }
 
@@ -25,6 +25,29 @@ public class Main {
         System.out.println(result);
     }
 
+    public static void subtract(Scanner sc) {
+        System.out.println("Enter numbers to subtract, enter = when finished.");
+        List<Float> items = getCalculations(sc);
+        float result = items.get(0);
+        for (int i = 1; i < items.size(); i++) result -= items.get(i);
+        System.out.println(result);
+    }
+
+    public static void multiply(Scanner sc) {
+        System.out.println("Enter numbers to multiply, enter = when finished.");
+        List<Float> items = getCalculations(sc);
+        float result = items.get(0);
+        for (int i = 1; i < items.size(); i++) result *= items.get(i);
+        System.out.println(result);
+    }
+
+    public static void divide(Scanner sc) {
+        System.out.println("Enter numbers to divide, enter = when finished.");
+        List<Float> items = getCalculations(sc);
+        float result = items.get(0);
+        for (int i = 1; i < items.size(); i++) result /= items.get(i);
+        System.out.println(result);
+    }
 
     public static List<Float> getCalculations(Scanner sc) {
         boolean finished = false;
